5e70c7c89078397b7744bf10af47733fdeb686ba
Shriprada M N <133126805+shripradamn1@users.noreply.github.com>
2023-08-14 12:47:13 +0530

    Update Rearrane.java

--- Rearrane.java
+++ Rearrane.java
@@ -1,1 +1,29 @@
+public class reaarageAlt {
+    public static void rearrange(long a[], int n){
+        long b[]=new long[n];
+        int count=0;
+        int mid=n/2;
+        for(int i=0;i<n;i++){
+           
+            if(i<n-1-i){
+                b[count]=a[n-i-1];
+                count++;
+            }
+             if(i<n-1-i){
+                b[count]=a[i];
+                count++;
+            }
+            
+            
+        }
+        if(n%2!=0)
+        b[n-1]=a[mid];
+        for(int i=0;i<n;i++){
+            a[i]=b[i];
+        }
+    
+        
+        
+    }
+}
 
