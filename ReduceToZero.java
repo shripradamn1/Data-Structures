b322b70d2359ba84bd400d9228f40ff2aa4c75ab
Shriprada M N <133126805+shripradamn1@users.noreply.github.com>
2023-08-16 19:01:01 +0530

    Create ReduceToZero.java

---
+++ ReduceToZero.java
@@ -0,0 +1,20 @@
+n =14
+  Output:6
+
+
+public int numberOfSteps(int n) {
+        
+        int count=0;
+        while(n>0){
+            if(n%2==0)
+            {
+                n=n/2;
+            }
+            else 
+            {
+                n=n-1;
+            }
+            count++;
+        }
+        return count;
+    }
